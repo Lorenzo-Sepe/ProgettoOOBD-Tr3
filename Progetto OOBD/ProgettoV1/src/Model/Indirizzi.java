2a2f89328332543a7a34645a54d48e7d1cf87268
Cavaliere <75066387+Lorenzo-Sepe@users.noreply.github.com>
2022-01-04 17:36:43 +0100

    Modello generato dal Class Diagramm

---
+++ Progetto OOBD/ProgettoV1/src/Model/Indirizzi.java
@@ -0,0 +1,32 @@
+package Model;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Indirizzi {
+
+	  private Boolean principale;
+	   private String via;
+	   private String città;
+	   private int codicePostale;
+	   private String nazione;
+	
+    /**
+     * Default constructor
+     */
+    public Indirizzi() {
+    }
+
+
+
+    /**
+     * 
+     */
+    public void setPrincipale() {
+        // TODO implement here
+    }
+
+}
---
+++ Progetto OOBD/ProgettoV1/src/Model/NumeriTelefonici.java
@@ -0,0 +1,23 @@
+package Model;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class NumeriTelefonici {
+
+	private String Prefisso;
+    private String Numero;
+    private String tipoNumero;
+    
+    /**
+     * Default constructor
+     */
+    
+    	
+    public void reindirizzamentoChiamate() {
+        // TODO implement here
+    }
+
+}
