109d7090d038259973bd09bb64e273bc5deaa37c
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-01-04 17:42:59 +0100

    Add files via upload

---
+++ Progetto OOBD/ProgettoV1/src/Model/Cassaforte.java
@@ -0,0 +1,25 @@
+package Model;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Cassaforte extends gruppo {
+
+	private String password;
+
+    /**
+     * Default constructor
+     */
+    public Cassaforte() {
+    }
+
+    /**
+     * 
+     */
+    
+
+
+}
