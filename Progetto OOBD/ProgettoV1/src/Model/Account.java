d621755f40d0880d3bc069b4f22b672310f92d1c
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2022-01-04 17:41:16 +0100

    Add files via upload

---
+++ Progetto OOBD/ProgettoV1/src/Model/Account.java
@@ -0,0 +1,26 @@
+package Model;
+
+/**
+ * 
+ */
+public class Account {
+
+    /**
+     * Default constructor
+     */
+    public Account() {
+    }
+
+    /**
+     * 
+     */
+    private String fornitore;
+    private String nickname;
+    private String benvenuto;
+    private Email mail=null;
+
+    public void gestioneDuplicati() {
+        // TODO implement here
+    }
+
+}
