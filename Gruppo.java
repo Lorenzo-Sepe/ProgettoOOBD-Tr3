db5ba9867603a97a91ffa672634736bde6441136
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-01-05 13:15:08 +0100

    Add files via upload

---
+++ Gruppo.java
@@ -0,0 +1,20 @@
+package Model;
+
+/**
+ * 
+ */
+public class Gruppo {
+
+    private String nome;
+    /**
+     * Default constructor
+     */
+    public Gruppo() {
+    }
+
+
+    public void AggiungiContatto() {
+        // TODO implement here
+    }
+
+}
