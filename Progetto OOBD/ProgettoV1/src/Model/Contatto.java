b6342d1cd6ca24582492e0a32ae6928c909eb6c3
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-01-05 13:17:20 +0100

    Add files via upload

--- Progetto OOBD/ProgettoV1/src/Model/Contatto.java
+++ Progetto OOBD/ProgettoV1/src/Model/Contatto.java
@@ -1,55 +1,83 @@
 package Model;
 
-import java.util.*;
+import java.util.ArrayList;
 
-/**
- * 
- */
 public class Contatto {
-
-    
-	private String prefisso;
-    private String nome;
-    private String cognome;
-    private String pathFoto;
-    private ArrayList<Email> mail=null;
-
-    /**
-     * Default constructor
-     */
- 
-	public Contatto() {
-    }
-    
-
-
-
-
-
-
-  public void Aggiungere contatto() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void Elimina contatto() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void Modifica contatto() {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public void Gestione duplicati() {
-        // TODO implement here
-    }
-
+	private int contatto_ID;
+	private String prefissoNome;
+	private String nome;
+	private String cognome;
+	private String pathFoto;
+	private ArrayList<Email> ListaEmail = null;
+	private ArrayList<Indirizzi> ListaIndirizzi = null;
+	private ArrayList<NumeriTelefonici> ListaNumeri = null;
+	private ArrayList<account> ListaAccount = null;
+	
+	public Contatto (String prefisso, String nome, String cognome, String pathFoto) {
+		if (prefisso == null) {
+			this.prefissoNome = null;
+		}
+		else {
+			this.prefissoNome = new String (prefisso);
+		}
+		this.nome = new String (nome);
+		this.cognome = new String (cognome);
+		if (pathFoto == null) {
+			this.pathFoto = null;
+		}
+		else {
+			this.pathFoto = new String (pathFoto);
+		}
+	}
+	
+	public void setID (int id) {
+		this.contatto_ID = id;
+	}
+	
+	public int getID () {
+		return contatto_ID;
+	}
+	
+	public void setPrefissoNome (String pref) {
+		this.prefissoNome = pref;
+	}
+	
+	public String getPrefissoNome () {
+		return prefissoNome;
+	}
+	
+	public void setNome (String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome () {
+		return nome;
+	}
+	
+	public void setCognome (String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getCognome () {
+		return cognome;
+	}
+	
+	public void setPathFoto (String PF) {
+		this.pathFoto = PF;
+	}
+	
+	public String getPathFoto () {
+		return pathFoto;
+	}
+	
+	public void AggiungiIndirizzo (Indirizzi i) {
+		ListaIndirizzi.add(i);
+	}
+	
+	public boolean EliminaIndirizzo (Indirizzi i) {
+		boolean riuscita;
+		riuscita = ListaIndirizzi.remove(i);
+		return riuscita;
+	}
 }
+
