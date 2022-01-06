eb483e1014223217ed5d594ee9c5093fd3f53f08
Cavaliere <75066387+Lorenzo-Sepe@users.noreply.github.com>
2022-01-06 20:00:21 +0100

    Add files via upload

---
+++ Progetto OOBD/ProgettoV1/src/implementazioneDAOpostgreSQL/ImplementazioneAccountDAO.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package implementazioneDAOpostgreSQL;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dao.AccountDAO;
+import dao.Connessione;
+import model.Account;
+import model.Contatto;
+
+/**
+ * @author LorenzoSepe
+ *
+ */
+public class ImplementazioneAccountDAO implements AccountDAO {
+	
+private Connection connection;
+	
+	public ImplementazioneAccountDAO() {
+		try {
+			connection = Connessione.getInstance().getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void addAccountDB(Account a, Contatto c) {
+		// TODO Auto-generated method stub
+		PreparedStatement addAccountPS;
+		try {
+			addAccountPS = connection.prepareStatement("INSERT INTO ACCOUNT (Contatto_Associato, Fornitore, Nickname, mail, Frase_Di_Benvenuto) (?,?,?,?,?)");
+			addAccountPS.setInt(1, c.getID());
+			addAccountPS.setString(2 , a.getFornitore());
+			addAccountPS.setString(3, a.getNickname());
+			addAccountPS.setString(4, a.getMail());
+			addAccountPS.setString(5, a.getBenvenuto());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public ArrayList<Account> leggiAccountDB(Contatto c) {
+			PreparedStatement leggiAccountPS;
+			ArrayList<Account> listaAccount = new ArrayList<>();		
+			try {
+				leggiAccountPS = connection.prepareStatement(
+						//da cambare una volta finito il database
+						"SELECT * FROM \"Account\".\"Contatto\" WHERE \"Contatto_Associato\"='"+c.getID()+"'");
+						
+			ResultSet rs = leggiAccountPS.executeQuery();
+			while (rs.next()) {
+				Account i = new Account (rs.getString("Fornitore"), rs.getString("Nickname"),rs.getString("Frase_di_Benvenuto"),  rs.getString("Mail") );
+				listaAccount.add(i);
+				connection.close();
+			}
+			rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return listaAccount;		
+		}
+
+}
+
--- Progetto OOBD/ProgettoV1/src/implementazioneDAOpostgreSQL/ImplementazioneIndirizziDAO.java
+++ Progetto OOBD/ProgettoV1/src/implementazioneDAOpostgreSQL/ImplementazioneIndirizziDAO.java
@@ -21,6 +21,7 @@ import model.Contatto;
 import model.Indirizzi;
 
 
+
 /**
  * @author LorenzoSepe
  *
@@ -40,14 +41,48 @@ public class ImplementazioneIndirizziDAO implements IndirizziDAO {
 
 	@Override
 	public void addIndirizzoDB(Indirizzi i, Contatto c) {
-		// TODO Auto-generated method stub
-
+		PreparedStatement addIndirizzoPS;
+		try {
+			addIndirizzoPS = connection.prepareStatement(
+					//da aggiunstare una volta finito il DB
+					"INSERT INTO INDIRIZZI (via , città, codice_postale, nazione) VALUES (?, ?, ?, ?)");
+					addIndirizzoPS.setString(1, i.getVia());
+					addIndirizzoPS.setString(2, i.getCittà());
+					addIndirizzoPS.setInt(3, i.getCodicePostale());
+					addIndirizzoPS.setString(4, i.getNazione());
+					ResultSet rs = addIndirizzoPS.executeQuery();
+					//- Release delle risorse
+					rs.close();
+					addIndirizzoPS.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public Indirizzi leggiIndirizziDB(Contatto c) {
+	public void addAbitaDB(Indirizzi i, Contatto c, String identificatore) {
+		PreparedStatement addAbitaPS;
+		try {
+			addAbitaPS = connection.prepareStatement("INSER INTO ABITA(indirizzo_ID, Contatto_ID, abitazione_principale, identificatore) (? , ? , ?, ?)");
+			addAbitaPS.setInt(1, i.getID());
+			addAbitaPS.setInt(2, c.getID());
+			addAbitaPS.setString(3, "FALSE");
+			addAbitaPS.setString(4, identificatore);
+			ResultSet rs = addAbitaPS.executeQuery();
+			//- Release delle risorse
+			rs.close();
+			addAbitaPS.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public ArrayList<Indirizzi> leggiIndirizziDB(Contatto c) {
 		PreparedStatement leggiIndirizziPS;
-		ListaIndirizzi l=new ListaIndirizzi();
+		ArrayList<Indirizzi> listaIndirizzi = new ArrayList<>();		
 		try {
 			leggiIndirizziPS = connection.prepareStatement(
 					//da cambare una volta finito il database
@@ -55,8 +90,8 @@ public class ImplementazioneIndirizziDAO implements IndirizziDAO {
 					
 		ResultSet rs = leggiIndirizziPS.executeQuery();
 		while (rs.next()) {
-			Indirizzi i = new Indirizzi (rs.getBoolean("Abitazione_Principale"),  rs.getString("Via"), rs.getString("Città"), rs.getInt("Codice_Postale"), rs.getString("Nazione"), rs.getString("Identificatore") );
-			l.addSocieta(i);
+			Indirizzi i = new Indirizzi (rs.getInt("Indirizzo_ID"), rs.getBoolean("Abitazione_Principale"),  rs.getString("Via"), rs.getString("Città"), rs.getInt("Codice_Postale"), rs.getString("Nazione"), rs.getString("Identificatore") );
+			listaIndirizzi.add(i);
 			connection.close();
 		}
 		rs.close();
@@ -65,7 +100,7 @@ public class ImplementazioneIndirizziDAO implements IndirizziDAO {
 			e.printStackTrace();
 		}
 		
-		return l;		
+		return listaIndirizzi;		
 	}
 
 }
