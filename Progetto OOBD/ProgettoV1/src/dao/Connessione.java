814148dba0a5fd8091d1ccbec44ceea932e1a2dc
Cavaliere <75066387+Lorenzo-Sepe@users.noreply.github.com>
2022-01-21 17:56:00 +0100

    aggiornamento per AWS

--- Progetto OOBD/ProgettoV1/src/dao/Connessione.java
+++ Progetto OOBD/ProgettoV1/src/dao/Connessione.java
@@ -1,15 +1,26 @@
 package dao;
 
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Connessione {
 
 	// ATTRIBUTI
 	private static Connessione istanza;
 	private Connection connection = null;
-	private String nome = "AccessoProgetto";
-	private String password = "BarraTramontana";
-	private String url = "jdbc:postgresql://localhost:5432/Progetto3";
+	
+	/*Per il testing in locale
+	  private String nome = "AccessoProgetto";
+	  private String password = "BarraTramontana";
+	 private String url = "jdbc:postgresql://localhost:5432/Progetto3";
+	 */
+	
+	private String url = "jdbc:postgresql://progetto.c95ssxjq5g6b.us-east-1.rds.amazonaws.com:5432/ProgettoOOBD";
+	private String nome = "LAR";
+	private String password = "UninaLAR";
 	private String driver = "org.postgresql.Driver";
 
 	// COSTRUTTORE
