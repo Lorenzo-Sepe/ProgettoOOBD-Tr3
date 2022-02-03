713c2e37f53d5545f253e40eb9a2d2727fe460ce
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-02-03 18:51:55 +0100

    Add files via upload

---
+++ Progetto OOBD/ProgettoV1/src/dao/ContattoDAO.java
@@ -0,0 +1,16 @@
+package DAO;
+
+import java.sql.Connection;
+import Model.Contatto;
+import java.util.ArrayList;
+
+public interface ContattoDAO {
+	
+	public void addContattoDB (Contatto c);
+	
+	public void deleteContattoDB (Contatto c);
+	
+	public void updateContattoDB (Contatto c, String prefisso, String nome, String cognome, String pathFoto);
+	
+	public ArrayList<Contatto> readContattoDB (Contatto c);
+}
