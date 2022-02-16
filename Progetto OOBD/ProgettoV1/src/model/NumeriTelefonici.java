d696d89156874af307f7c17c5e9feeac852911dc
Cavaliere <75066387+Lorenzo-Sepe@users.noreply.github.com>
2022-02-16 20:22:48 +0100

    Update NumeriTelefonici.java

--- Progetto OOBD/ProgettoV1/src/model/NumeriTelefonici.java
+++ Progetto OOBD/ProgettoV1/src/model/NumeriTelefonici.java
@@ -1,67 +1,75 @@
-package model;
+package Model;
 
 
 public class NumeriTelefonici {
-;
+	private int numeroID;
 	private String tag;
-	private String prefisso;
-    private String numero;
-    private String tipoNumero;
-    private NumeriTelefonici deputato;
-    
-    public NumeriTelefonici (String tag,String Prefisso, String Numero, String tipoNumero) {
-    	this.tag=tag;
-    	this.prefisso = Prefisso;
-    	this.numero = Numero;
-    	this.tipoNumero = tipoNumero;
+        private String prefisso;
+        private String numero;
+        private String tipoNumero;
+        private NumeriTelefonici deputato;
+
+    public NumeriTelefonici (int NumeroID, String Prefisso, String Numero, String Tag, String tipoNumero) {
+        this.numeroID=NumeroID;
+    	this.tag=Tag;
+        this.prefisso = Prefisso;
+        this.numero = Numero;
+        this.tipoNumero = tipoNumero;
     }
+
+    public void setID(int NumeroID) {
+    	this.numeroID=NumeroID;
+    }
+    
+   public int getID(){
+	   return numeroID;
+   }
+
     
     public void setPrefisso (String Prefisso) {
-    	this.prefisso = Prefisso;
+        this.prefisso = Prefisso;
     }
-    
+
     public String getPrefisso () {
-    	return prefisso;
+        return prefisso;
     }
-    
+
     public void setNumero (String Numero) {
-    	this.numero = Numero;
+        this.numero = Numero;
     }
-    
+
     public String getNumero () {
-    	return numero;
+        return numero;
     }
-    
+
     public void setTipoNumero (String tipoNumero) {
-    	this.tipoNumero = tipoNumero;
+        this.tipoNumero = tipoNumero;
     }
-    
+
     public String getTipoNumero () {
-    	return tipoNumero;
+        return tipoNumero;
     }
-    
+
     public boolean setDeputato (NumeriTelefonici deputato) {
-    	boolean riuscita;
-    	String tipo1 = new String (this.getTipoNumero());
-    	String tipo2 = new String (deputato.getTipoNumero());
-    	if(tipo1.equals(tipo2)==false) {
-    		this.deputato = deputato;
-    		riuscita = true;
-    	}
-    	else {
-    		riuscita = false;
-    	}
-    	return riuscita;
+        boolean riuscita;
+        String tipo1 = new String (this.getTipoNumero());
+        String tipo2 = new String (deputato.getTipoNumero());
+        if(tipo1.equals(tipo2)==false) {
+            this.deputato = deputato;
+            riuscita = true;
+        }
+        else {
+            riuscita = false;
+        }
+        return riuscita;
     }
 
-	public String stampaNumero() {
-		return prefisso+numero+" tipo: "+tipoNumero ;
-	}
+    public String stampaNumero() {
+        return prefisso+numero+" tipo: "+tipoNumero ;
+    }
 
-	public String  gettag() {
-		return tag;
-	}
+    public String  getTag() {
+        return tag;
+    }
 
-	
-    
 }
