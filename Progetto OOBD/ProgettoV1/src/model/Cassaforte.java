f3a9469e075214fa341a21a9c74099490b5984cb
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2022-02-16 17:02:27 +0100

    Update Cassaforte.java

--- Progetto OOBD/ProgettoV1/src/model/Cassaforte.java
+++ Progetto OOBD/ProgettoV1/src/model/Cassaforte.java
@@ -1,13 +1,9 @@
 package Model;
 
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
+
 import java.util.*;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 
 /**
  *
@@ -41,47 +37,8 @@ import javax.crypto.spec.PBEKeySpec;
 			contatti.addAll(arraylist);
 		}
 	}
-    //questo script è per fare dei test
     public void setPassword(String pass) {
-//    	byte [] salt= "E*.^%j=+Z6-Ed9c,".getBytes();
-//    	KeySpec spec = new PBEKeySpec(pass.toCharArray(), salt,1,16*8);
-//    	SecretKeyFactory f =SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-//    	password=f.generateSecret(spec).getEncoded().toString();
     	password=pass;
     	}
 
-    public  String CifraturaPassword(String pass) {
-    	// TODO gestire veramente la cifrattura
-    	byte [] salt= "E*.^%j=+Z6-Ed9c,".getBytes();
-    	KeySpec spec = new PBEKeySpec(pass.toCharArray(), salt,1,16*8);
-
-		try {
-			SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-			return f.generateSecret(spec).getEncoded().toString();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	  return pass;
-    }
-
-//    public ArrayList<Contatto> getListaGruppo(String pass){
-//		if(pass.compareTo(password)==0) {
-//			System.out.println("yess ti do il gruppo");
-//			return contatti;
-//		}else {
-//			System.out.println("Noooo password errata, niente gruppo >-<");
-//			return contatti;
-//		}
-//	}
-    
-    public byte[] getEncryptedPassword(String password,byte[] salt,int iterations,int derivedKeyLength) throws NoSuchAlgorithmException, InvalidKeySpecException
-	{
-		KeySpec spec = new PBEKeySpec(password.toCharArray(),salt,iterations,derivedKeyLength * 8);
-		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-		return f.generateSecret(spec).getEncoded();
-	}
 }
