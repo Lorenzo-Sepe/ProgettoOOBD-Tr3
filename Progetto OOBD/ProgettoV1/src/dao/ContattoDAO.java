6209e05f4b0de9b634222034113757078a6989fe
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-02-21 18:16:37 +0100

    Add files via upload
    aggiornato il contattoDAO

--- Progetto OOBD/ProgettoV1/src/dao/ContattoDAO.java
+++ Progetto OOBD/ProgettoV1/src/dao/ContattoDAO.java
@@ -18,12 +18,20 @@ public interface ContattoDAO {
 	public void updateContattoDB (Contatto c, String prefisso, String nome, String cognome, String pathFoto);
 	
 	public ArrayList<Contatto> readContattoDB (Contatto c);
-	
-	public ArrayList<Contatto> selectAllDB ();
+
+	public int addNumeriDB (int idContatto, String prefisso, String numero, String tag, String tipo);
 	
 	public ArrayList<NumeriTelefonici> getListaNumeri (int id);
 	
+	public int addIndirizziDB (int idContatto, String via, String città, String codicePostale, String nazione, String tag, boolean principale );
+	
 	public ArrayList<Indirizzi> getListaIndirizzi (int id);
 	
+	public int addAccountDB (int idContatto ,String nickname, String fornitore, String benvenuto, String email);
+	
 	public ArrayList<Account> getListaAccount (int id);
+	
+	public void addEmailDB (int idContatto, String email);
+	
+	public  ArrayList<String> getListaEmail (int id);
 }
