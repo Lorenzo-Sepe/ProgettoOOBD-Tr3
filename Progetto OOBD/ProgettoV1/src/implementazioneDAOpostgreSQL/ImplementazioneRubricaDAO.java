6d134ec656d2198a46d3010792403cb33793172d
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-02-24 19:32:31 +0100

    creato metodo addGruppo

--- Progetto OOBD/ProgettoV1/src/implementazioneDAOpostgreSQL/ImplementazioneRubricaDAO.java
+++ Progetto OOBD/ProgettoV1/src/implementazioneDAOpostgreSQL/ImplementazioneRubricaDAO.java
@@ -19,6 +19,7 @@ public class ImplementazioneRubricaDAO implements RubricaDAO {
 			e.printStackTrace();
 		}
 	}
+	
  public void  chiudiConnessione() {
 	 try {
 		connection.close();
@@ -27,10 +28,11 @@ public class ImplementazioneRubricaDAO implements RubricaDAO {
 		e.printStackTrace();
 	}
  }
-	public ArrayList<Contatto> selectAllDB () throws SQLException {
+ 
+	public ArrayList<Contatto> selectAllDB () {
 		PreparedStatement selectAllPS;
 		ArrayList<Contatto> listaContatti = new ArrayList<>();
-		
+		try {
 			selectAllPS = connection.prepareStatement("SELECT * FROM Contatto ORDER BY contatto_id");
 			ResultSet rs = selectAllPS.executeQuery();
 			while (rs.next()) {
@@ -40,6 +42,11 @@ public class ImplementazioneRubricaDAO implements RubricaDAO {
 				
 			}
 			}
+			
+	}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 		return listaContatti;
 	}
@@ -48,34 +55,30 @@ public class ImplementazioneRubricaDAO implements RubricaDAO {
 	public int addContattoDB(String prefisso, String nome, String cognome, String path) throws SQLException {
 		 PreparedStatement addContattoPS;
          int id=0;
-            addContattoPS = connection.prepareStatement("INSERT INTO Contatto (prefisso_nome,nome,cognome,path_foto,visibilitÃ ) VALUES (?,?,?,?,?) returning Contatto_id as ID");
+            addContattoPS = connection.prepareStatement("INSERT INTO Contatto (prefisso_nome,nome,cognome, \"path_foto\") VALUES (?,?,?,?) returning Contatto_id as ID");
             addContattoPS.setString(1, prefisso);
             addContattoPS.setString(2, nome);
             addContattoPS.setString(3, cognome);
             addContattoPS.setString(4, path);
-            addContattoPS.setBoolean(5, true);
             ResultSet rs= addContattoPS.executeQuery();
-            rs.next();
+             rs.next();
             id= rs.getInt("ID");
-            
-
-      
+       
         return id;
 
 	}
-
-	public Contatto readContattoDB (int  contattoID) throws Exception {
-		PreparedStatement readContattoPS;
-		Contatto contatto=null;
-		
-			readContattoPS = connection.prepareStatement("SELECT * FROM Contatto WHERE contatto_id = ?");
-			readContattoPS.setInt(1, contattoID);
-			ResultSet rs = readContattoPS.executeQuery();
-			if(rs.next()==false) throw new Exception("Riga non trovata");
-				contatto= new Contatto (rs.getInt("contatto_id"), rs.getString("prefisso_nome"), rs.getString("nome"), rs.getString("cognome"),rs.getString("path_foto"));
-				
-		
-		
-		return contatto;
+	
+	public void addGruppoDB (String nomeGruppo, ArrayList<Integer> membriGruppo) throws SQLException {
+		PreparedStatement addGruppoDB;
+		addGruppoDB = connection.prepareStatement("INSERT INTO gruppi VALUES (?)");
+		addGruppoDB.setString(1, nomeGruppo);
+		addGruppoDB.execute();
+		for (int membro : membriGruppo) {
+			addGruppoDB = connection.prepareStatement("INSERT INTO appartenenza VALUES (?,?)");
+			addGruppoDB.setString(1, nomeGruppo);
+			addGruppoDB.setInt(2, membro);
+			addGruppoDB.execute();
+		}
 	}
+
 }
