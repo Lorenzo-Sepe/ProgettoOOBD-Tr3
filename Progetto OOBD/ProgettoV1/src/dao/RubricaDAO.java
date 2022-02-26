14db71430b9f7a69de0699ef29f7ad80bd32f9c1
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-02-26 17:09:32 +0100

    Aggiunto metodo deleteGroupDB

--- Progetto OOBD/ProgettoV1/src/dao/RubricaDAO.java
+++ Progetto OOBD/ProgettoV1/src/dao/RubricaDAO.java
@@ -4,14 +4,19 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import Model.Contatto;
+import Model.Gruppo;
 
 public interface RubricaDAO {
 
 
     public ArrayList<Contatto> selectAllDB ();
     
+    public ArrayList<Gruppo> selectListaGruppiDB () throws SQLException;
+    
     public int  addContattoDB (String prefisso, String nome,String cognome,String path) throws SQLException;
     
     public void addGruppoDB (String nomeGruppo, ArrayList<Integer> membriGruppo) throws SQLException;
+    
+    public void deleteGruppoDB (String nomeGruppo) throws SQLException;
 
 }
