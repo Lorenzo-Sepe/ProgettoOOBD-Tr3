c3d0c2a18bd3631d9c3bccd9d56caab6804f7f48
Cavaliere <75066387+Lorenzo-Sepe@users.noreply.github.com>
2022-03-01 13:41:03 +0100

    fix sintassi comandi SQL

--- Progetto OOBD/ProgettoV1/src/implementazioneDAOpostgreSQL/ImplementazioneAccountDAO.java
+++ Progetto OOBD/ProgettoV1/src/implementazioneDAOpostgreSQL/ImplementazioneAccountDAO.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package implementazioneDAOpostgreSQL;
+package ImplementazioneDAOpostgreSQL;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -19,7 +19,7 @@ import Model.Contatto;
  *
  */
 public class ImplementazioneAccountDAO implements AccountDAO {
-
+	
 private Connection connection;
 	
 	public ImplementazioneAccountDAO() {
@@ -30,28 +30,19 @@ private Connection connection;
 			e.printStackTrace();
 		}
 	}
-	public void  chiudiConnessione() {
-		 try {
-			connection.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	 }
 
 	@Override
 	public void addAccountDB(Account a, Contatto c) {
 		// TODO Auto-generated method stub
 		PreparedStatement addAccountPS;
 		try {
-			addAccountPS = connection.prepareStatement("INSERT INTO ACCOUNT (Contatto_Associato, Fornitore, Nickname, mail, Frase_Di_Benvenuto) (?,?,?,?,?)");
+			addAccountPS = connection.prepareStatement("INSERT INTO ACCOUNT (Contatto_Associato, Fornitore, Nickname, mail, Frase_Di_Benvenuto) (?,?,?,?,?);");
 			addAccountPS.setInt(1, c.getID());
 			addAccountPS.setString(2 , a.getFornitore());
 			addAccountPS.setString(3, a.getNickname());
 			addAccountPS.setString(4, a.getMail());
 			addAccountPS.setString(5, a.getBenvenuto());
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -63,14 +54,12 @@ private Connection connection;
 			try {
 				leggiAccountPS = connection.prepareStatement(
 						//da cambare una volta finito il database
-						"SELECT * FROM \"Account\".\"Contatto\" WHERE \"Contatto_Associato\"='"+c.getID()+"'");
+						"SELECT * FROM \"Account\".\"Contatto\" WHERE \"Contatto_Associato\"='"+c.getID()+"';");
 						
 			ResultSet rs = leggiAccountPS.executeQuery();
 			while (rs.next()) {
 				Account i = new Account (rs.getString("Fornitore"), rs.getString("Nickname"),rs.getString("Frase_di_Benvenuto"),  rs.getString("Mail") );
 				listaAccount.add(i);
-
-
 			}
 			rs.close();
 			} catch (SQLException e) {
