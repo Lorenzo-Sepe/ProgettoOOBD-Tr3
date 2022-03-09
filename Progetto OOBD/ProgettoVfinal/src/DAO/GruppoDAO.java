0ed8ec1991e5bece5a5c8d4b44ea608f59be99e5
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-03-09 18:46:51 +0100

    Add files via upload

--- Progetto OOBD/ProgettoVfinal/src/DAO/GruppoDAO.java
+++ Progetto OOBD/ProgettoVfinal/src/DAO/GruppoDAO.java
@@ -7,7 +7,20 @@ import Model.Contatto;
 
 public interface GruppoDAO {
 
-	public ArrayList<Contatto> selectListaContattiGruppoDB (String nomeGruppo) throws SQLException;
-	
-	public boolean checkContattoInGruppo (int idContatto);
+	/**
+	 * esegue una query sulla tabella Gruppo del database <br> 
+	 * restituisce tutti i contatti associati del gruppo con il nome uguale a parametro dato
+	 * @param nomeGruppo String del nome 
+	 * @return ArrayList{@literal<Contatto>} dato dal ResulSet della query
+	 * @throws SQLException
+	 * @see {@link Model.Contatto}
+	 */
+    public ArrayList<Contatto> selectListaContattiGruppoDB (String nomeGruppo) throws SQLException;
+
+    /**
+     * esegue una query sulla tabella Gruppo del database per verificare se un contatto fa parte di un gruppo
+     * @param idContatto int codice che identifica univocamente il contatto
+     * @return boolean dato dal ResulSet della query
+     */
+    public boolean checkContattoInGruppo (int idContatto);
 }
