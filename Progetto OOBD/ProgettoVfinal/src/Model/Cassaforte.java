6ba673df114bda0ee2ecca99346384814a52884f
Trincalex <96735976+Trincalex@users.noreply.github.com>
2022-03-09 15:52:17 +0100

    Add files via upload

--- Progetto OOBD/ProgettoVfinal/src/Model/Cassaforte.java
+++ Progetto OOBD/ProgettoVfinal/src/Model/Cassaforte.java
@@ -21,36 +21,54 @@ import java.util.*;
     }
 
 
+    /**
+     * restituisce la password della cassaforte
+     * @return Stringa che rappresenta la password
+     */
     public String getPassword(){
     	return password;
     }
     
+    /**
+     * restituisce la lista dei contatti della cassaforte
+     * @return ArrayList di Contatto che rappresenta la lista di contatti
+     */
     public ArrayList<Contatto> getListaGruppo(){
     	return contatti;
     }
-    
+    /**
+     * aggiunge un contatto in cassaforte
+     * @param i
+     */
     public void aggiungiContatto (Contatto i) {
     	contatti.add(i);
 	}
     
-//    public void aggiungiListaContatti (ArrayList<Contatto> listaContatti) {
-//    	this.contatti.addAll(listaContatti);
-//    }
-    
-	
+	/**
+	 * imposta la password della cassaforte
+	 * @param pass Stringa che rappresenta la password da impostare
+	 */
     public void setPassword(String pass) {
     	password=pass;
     	}
 
 
+    /**
+     * imposta la lista di contatti della cassaforte
+     * @param contattiInCassaforte ArrayList di contatti che rappresenta la lista da impostare
+     */
 	public void setListaContatti(ArrayList<Contatto> contattiInCassaforte) {
 		contatti = contattiInCassaforte;		
 	}
-
-
-	public Contatto getContatto(int contattoID) {
+	
+	/**
+	 * restituisce un contatto dalla cassaforte
+	 * @param id identificatore del contatto da restituire
+	 * @return oggetto della classe Contatto rappresentante il contatto da restituire
+	 */
+	public Contatto getContatto (int id) {
 		for (Contatto contatto : contatti) {
-			if (contatto.getID()==contattoID) {
+			if (contatto.getID()==id) {
 				return contatto;
 			}
 		}
